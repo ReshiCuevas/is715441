936e785b12c427a143e380f2754a417c90d49299
RickWithNoMorty <39529945+RickWithNoMorty@users.noreply.github.com>
2018-05-31 03:04:11 -0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+	char nombre[15];
+	float calificacion;
+} Profesor;
+
+float averageArray(Profesor ______  , int _____);
+void readArray(Profesor arr, Profesor *parr int y){
+  int i,;
+  Profesor rating, *apu;
+  *apu = &x;
+  for(i=0;i<y;i++){
+    fgets(bleh, sizeof(bleh), stdin);
+    scanf("%f", &j);
+    *parr->arr[i].nombre = bleh;
+    *parr->arr[i].calificacion = j;
+    parr++;
+
+  }
+
+  }
+}
+void mergeArrays(Profesor _____ , int _____, Profesor ____, int _____, Profesor _____, int ____);
+void sortArray(Profesor _____, int ____);
+void printArray(Profesor ____ , int _____);
+
+
+void main(){
+	Profesor arr1[20];  //Primer arreglo
+	Profesor arr2[20];  //Segundo arreglo
+	Profesor arrF[40];  //Arreglo final, con elementos fusionados y ordenados
+Profesor arr1[20], *apu;  
+	Profesor arr2[20];  
+	Profesor arrF[40]; 
+	*apu = &arr1;
+	int n1, n2; 
+  puts("¿Cuantos profesores se van agregar? \n");
+  scanf(" %d \n", &n1);
+  readArray(&arr1, *apu, n1);
+readArray(______________); //leer el segundo arreglo 
+mergeArrays(_______________________________);  //Fusionar los dos arreglos en un tercer arreglo
+
+sortArray(_____________);  //Ordenar los elementos del tercer arreglo, recuerde que pueden 
+  //existir profesores repetidos
+
+printArray(___________);   //Imprimir el resultado final
+
+return 0;
+}
